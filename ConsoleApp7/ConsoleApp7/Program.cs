724a41d73872ac38cc18fb9901eef40b4a82bf92
Anzerl <41679387+Anzerl@users.noreply.github.com>
2019-04-04 13:29:23 +0800

    Add files via upload

---
+++ ConsoleApp7/ConsoleApp7/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static public void Read(string path)
+        {
+            StreamReader sr = new StreamReader(path, Encoding.Default);
+            String line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                Console.WriteLine(line.ToString());
+            }
+        }
+        static void Main()
+        {
+            string path = "E:\\input.txt";
+
+            int lines = 0;  //用来统计txt行数
+            FileStream fs = new FileStream(path, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            StreamReader sr = new StreamReader(fs);
+            while (sr.ReadLine() != null)
+            {
+                lines++;
+            }
+            Console.WriteLine(lines);
+            Console.ReadKey();
+
+        }
+
+
+    }
+
+
+}
+
+
